2f740c8d8ca5eb3eac4b1cc69aae5ffc0811989d
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-06-27 20:01:03 +0300

    small fix

--- package/src/theme_switcher/components/theme_switcher.tsx
+++ package/src/theme_switcher/components/theme_switcher.tsx
@@ -37,7 +37,7 @@ export default function ThemeSwticher({ isDark, className, lightLabelText, darkL
         aria-label={isDarkMode ? lightLabelText : darkLabelText}
         className={cn(
             "relative flex items-center justify-center", // Base flex container for centering content
-            "p-2.5 rounded-full cursor-pointer overflow-hidden", // Hide overflow for potential animations, pointer cursor
+            "p-2.5 aspect-square h-11.5 rounded-full cursor-pointer overflow-hidden", // Hide overflow for potential animations, pointer cursor
             "transition-colors duration-300 ease-in-out", // Smooth transition for background color
             "outline-none ring-2 ring-offset-2 ring-opacity-75", // Focus ring for accessibility
             "bg-gradient-to-br from-blue-300 to-blue-500 text-yellow-500 shadow-lg shadow-blue-500/50 ring-green-500", // Light mode default
