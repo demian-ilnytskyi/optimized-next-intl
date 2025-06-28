f53820c62bb19694ef9f66f14fd0f9e804bab0b6
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-06-28 23:08:19 +0300

    small improvments

--- package/dist/src/theme_switcher/components/theme_switcher.js
+++ package/dist/src/theme_switcher/components/theme_switcher.js
@@ -27,7 +27,7 @@ export default function ThemeSwticher({ isDark, className, lightLabelText, darkL
             " transition-colors duration-300 ease-in-out" + // Smooth transition for background color
             " outline-none ring-2 ring-offset-2 ring-opacity-75" + // Focus ring for accessibility
             " bg-gradient-to-br from-blue-300 to-blue-500 text-yellow-500 shadow-lg shadow-blue-500/50 ring-green-500" + // Light mode default
-            " dark:from-gray-700 dark:to-gray-900 dark:text-yellow-300 dark:shadow-gray-900/50 dark:ring-yellow-500" + // Dark mode styles
+            " dark:from-gray-700 dark:to-gray-900 dark:text-yellow-300 dark:shadow-gray-900/50 dark:ring-yellow-500 " + // Dark mode styles
             className // Custom classes passed via props
         , children: [_jsx(Sun, { className: "transition-transform duration-500 ease-in-out" +
                     " rotate-0 scale-100 opacity-100 cursor-pointer" + // Default (light mode) state
