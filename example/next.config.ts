6562e9478c730a81a7940ae9c3911a3b960b25a5
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-06-30 10:22:42 +0300

    small refactor

--- example/next.config.ts
+++ example/next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"; import path from 'path';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+import path from 'path';
 
 const isDev = process.env.NODE_ENV === "development";
 
